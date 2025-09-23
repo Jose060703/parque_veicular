ef66015e5794b932d11aafa3ace1fc379bf12346
Joselito <jcortesanzurez@gmail.com>
2025-09-23 12:47:07 -0600

    hola soy yo

--- configuracionbd.php
+++ configuracionbd.php
@@ -7,6 +7,6 @@ $db   = "parque_veicular";
 $conn = new mysqli($host, $user, $pass, $db);
 
 if($conn->connect_error){
-    die("Error de conexión: IB  gpi" . $conn->connect_error);
+    die("Error de conexión:hola soy yo" . $conn->connect_error);
 }
 ?>
